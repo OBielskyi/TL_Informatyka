6adbb7bca7c6f3c7a8e54e149e000b6758fdebdd
Oleksandr Bielskyi <amyntwr@gmail.com>
2026-03-25 10:33:22 +0100

    Dodać zadanie stos
    Implement a stack in Python that processes character input and supports push and pop operations.

---
+++ stos.py
@@ -0,0 +1,19 @@
+# Zadanie - Stos w Python
+
+stos = []
+save = []
+
+while True:
+  print ("Program czyta literał znaków i wykonuje rozkazy.\n")
+  print ("Dowolny znak - operacja umieszczenia,\nGwiazdka (*) - pobranie elementu ze stosu.")
+  print ("Polecenie 'exit' (bez cudzysłowu) zamyka program.\n")
+  string = input ("Wprowadź literał znaków: ")
+  strlen = len (string)
+  if string == "exit":
+    exit()
+  for i in range (strlen):
+    if string[i] == "*" and len (stos) > 0:
+      save.append (stos.pop())
+    else:
+      stos.append (string[i])
+  print (save)
