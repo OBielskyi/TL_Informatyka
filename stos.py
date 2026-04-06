6335562a1c0bbc0089978a72bd02ee2a044b75f6
Oleksandr Bielskyi <amyntwr@gmail.com>
2026-04-06 21:24:25 +0200

    minor fix stos.py
    Gwiazdka (*) jako znak specjalny (nie pozwalamy na dodanie jej do stosu)

--- stos.py
+++ stos.py
@@ -4,18 +4,20 @@ stos = []
 save = []
 
 while True:
-  print ("Program czyta literał znaków i wykonuje rozkazy.\n")
-  print ("Dowolny znak - operacja umieszczenia,\nGwiazdka (*) - pobranie elementu ze stosu.")
-  print ("Polecenie 'exit' (bez cudzysłowu) zamyka program.\n")
-  string = input ("Wprowadź literał znaków: ")
-  strlen = len (string)
-  if string == "exit":
-    exit()
-  for i in range (strlen):
-    if string[i] == "*" and len (stos) > 0:
-      save.append (stos.pop())
-    else:
-      stos.append (string[i])
-  print (save)
-  stos = []
-  save = []
+    print("Program czyta literał znaków i wykonuje rozkazy.\n")
+    print(
+        "Dowolny znak - operacja umieszczenia,\nGwiazdka (*) - pobranie elementu ze stosu."
+    )
+    print("Polecenie 'exit' (bez cudzysłowu) zamyka program.\n")
+    string = input("Wprowadź literał znaków: ")
+    strlen = len(string)
+    if string == "exit":
+        exit()
+    for i in range(strlen):
+        if string[i] == "*" and len(stos) > 0:
+            save.append(stos.pop())
+        elif string[i] != "*":
+            stos.append(string[i])
+    print(save)
+    stos = []
+    save = []
