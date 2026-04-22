481d0e54d7e6a75d24b770e4f2c65fce82460d07
Oleksandr Bielskyi <amyntwr@gmail.com>
2026-04-22 10:43:25 +0200

    Add sleep function to pause output
    Added a sleep function to pause execution for 5 seconds.

--- imie_kolory.py
+++ imie_kolory.py
@@ -1,5 +1,6 @@
 # Oleksandr Bielskyi, 1Ic
 # Zadanie "Imię z kolorami"
+from time import sleep
 
 print("\033[1;34m        @  @\033[0m")
 print("\033[1;33m.---ooO-(_)-Ooo------------------------.")
@@ -8,3 +9,4 @@ print("|\033[1;42m                \033[1;31m\033[4mOleksandr Bielskyi\033[24m\03
 print("\033[1;49m|\033[1;42m   \033[1;49m.ooO\033[1;42m   \033[1;49mOoo.\033[1;42m                        \033[1;49m|")
 print("'---(  )---(  )------------------------'")
 print("     (_)    (_)")
+sleep(5)
